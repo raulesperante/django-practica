ff4cf42fb93306afc56e6c104e2df170e31ede5e
Raúl Esperante <ragusmat@hotmail.com>
2020-03-05 22:05:45 -0300

    First commit

---
+++ refugio/apps/mascota/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from apps.mascota.models import Vacuna, Mascota
+
+admin.site.register(Vacuna)
+admin.site.register(Mascota)
---
+++ refugio/apps/mascota/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from apps.adopcion.models import Persona
+
+class Vacuna(models.Model):
+    nombre = models.CharField(max_length=50)
+
+
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=50)
+    edad_aproximada = models.IntegerField()
+    fecha_recate = models.DateField()
+    persona = models.ForeignKey(Persona, null=True, 
+                                blank=True, on_delete=models.CASCADE)
+    vacuna = models.ManyToManyField(Vacuna)
